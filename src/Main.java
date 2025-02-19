ab1069faab8502eb1ee20408adeaa266eaf7fb93
Amir3zz <hunicastt@gmail.com>
2025-02-19 19:25:15 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -31,16 +31,6 @@ public class Main {
 
     public static void bubbleSort1(ArrayList<Integer> arr) {
         int n = arr.size();
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (arr.get(j) > arr.get(j + 1)) {
-                    int temp = arr.get(j);
-                    arr.set(j, arr.get(j + 1));
-                    arr.set(j + 1, temp);
-                }
-            }
-        }
     }
     public static void main(String[] args){
         Runnable test = () -> iterateCubes(5);
